9d3ef89d3768f7fba3fc755bdfa8dc7c42c33a3f
Moslem Mousazadeh <moslemmousazadeh@yahoo.com>
2020-09-25 17:59:34 +0330

    Create component about, change component Course

---
+++ src/app/Course.Component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'Course',
+  templateUrl: './Course.Component.html',
+})
+
+export  class CourseComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CourseComponent } from './Course.Component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CourseComponent, AboutComponent
   ],
   imports: [
     BrowserModule,
