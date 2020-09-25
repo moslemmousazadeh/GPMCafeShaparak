9ce7161454df56b9eb4a321e09dfbe147dde002d
Moslem Mousazadeh <moslemmousazadeh@yahoo.com>
2020-09-25 18:19:35 +0330

    change component home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '<h3>Moslem Tamplate</h3>',
+  /* template: `
+  <h3>Moslem</h3>
+  <h2>Mousazadeh</h2>
+  `, */
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './Course.Component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent, CourseComponent, AboutComponent
+    AppComponent, CourseComponent, AboutComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
